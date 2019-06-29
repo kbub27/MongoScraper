a7ac6c66f9ade3d336b53a7cf4c29e58dcb42de0
kbub27 <kalebmerriman27@gmail.com>
2019-06-28 20:09:00 -0700

    handlebars setup on server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const bars = require('express-handlebars');
 
 // Mongo uri
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoScraper';
 
 // Initialize express
 const app = express();
+
+// Handlebars setup 
+app.engine('handlebars', bars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Middleware
 app.use(logger('dev'));
@@ -15,6 +20,5 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Mongo connection
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-require('./models')(app);
